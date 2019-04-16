439104a1e656bf5e31cd067ae5bccc663a725ed4
devin.yeen@icloud.com <devin.yeen@icloud.com>
2019-04-16 09:56:46 +0800

    update traders

--- src/com/spread/ydy/java8/chap5/TraderSAndrTransactions.java
+++ src/com/spread/ydy/java8/chap5/TraderSAndrTransactions.java
@@ -3,6 +3,7 @@ package com.spread.ydy.java8.chap5;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class TraderSAndrTransactions {
@@ -21,12 +22,71 @@ public class TraderSAndrTransactions {
                 new Transaction(mario, 2012, 700),
                 new Transaction(alan, 2012, 950)
                 );
-        // 找出2011所有交易并按交易额排序
-        List<Transaction> tr2001 = transactions
-                .stream()
+        // 5.1找出2011所有交易并按交易额排序
+        List<Transaction> tr2001 = transactions.stream()
                 .filter(transaction -> transaction.getYear() == 2011)
                 .sorted(Comparator.comparing(Transaction::getValue))
                 .collect(Collectors.toList());
+        System.out.println(tr2001);
+
+        // 5.2交易员分别都在哪些不同城市工作过
+        List<String> cities = transactions.stream()
+                .map(transaction -> transaction.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(cities);
+
+        // 5.3查找所有来自于剑桥的交易员 并按姓名排序
+        List<Trader> cambridges = transactions.stream()
+                .map(Transaction::getTrader)
+                .filter(trader->trader.getCity().equals("Cambridge"))
+                .distinct()
+                .sorted(Comparator.comparing(Trader::getName))
+                .collect(Collectors.toList());
+        System.out.println(cambridges);
+
+        // 5.4.1 返回所有交易员姓名字符串 按字母顺序排序
+//        List<String> traders = transactions.stream()
+//                .map(Transaction::getTrader)
+//                .map(Trader::getName)
+//                .distinct()
+//                .sorted()
+//                .collect(Collectors.toList());
+        // 5.4.2 返回所有交易员姓名字符串 按字母顺序排序
+//        String traders = transactions.stream()
+//                .map(transaction -> transaction.getTrader().getName())
+//                .distinct()
+//                .sorted()
+//                .reduce("", (n1, n2) -> n1 + n2);
+//        System.out.println(traders);
+        // 5.4.3 返回所有交易员姓名字符串 按字母顺序排序
+        String traders = transactions.stream()
+                .map(transaction -> transaction.getTrader().getName())
+                .distinct()
+                .sorted()
+                .collect(Collectors.joining());
+        System.out.println(traders);
+
+        // 5.5 返回所有交易员姓名字符串 按字母顺序排序
+        boolean milan = transactions.stream()
+                .anyMatch(transaction -> transaction.getTrader().getCity().equals("Milan"));
+        System.out.println(milan);
+
+        // 5.6 打印生活在剑桥的交易员的所有交易额
+        transactions.stream()
+                .filter(transaction -> transaction.getTrader().getCity().equals("Cambridge"))
+                .map(transaction->transaction.getValue())
+                .forEach(System.out::println);
+
+        // 5.7.1 所有叫哟中最高的交易是多少
+        Optional<Integer> maxTr = transactions.stream()
+                .map(Transaction::getValue)
+                .reduce(Integer::max);
+        System.out.println(maxTr.get());
+        // 5.7.2 所有叫哟中最高的交易是多少
+        Optional<Transaction> maxTr1 = transactions.stream()
+                .max(Comparator.comparing(Transaction::getValue));
+        System.out.println(maxTr1.get().getValue());
     }
 
 }
