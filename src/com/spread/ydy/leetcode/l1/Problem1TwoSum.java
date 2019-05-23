60f0f92a1c3391cd223937f84baaa522434a8a8b
devin.yeen <devin.yeen@icloud.com>
2019-05-24 02:39:09 +0800

    Leetcode TwoSum 2019/05/24 02:38

---
+++ src/com/spread/ydy/leetcode/l1/Problem1TwoSum.java
@@ -0,0 +1,63 @@
+package com.spread.ydy.leetcode.l1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// Level:1
+public class Problem1TwoSum {
+    private static int[] nums   = { 2, 7, 11, 15 };
+    private static int   target = 9;
+
+    public static void main(String[] args) {
+        Problem1TwoSum s = new Problem1TwoSum();
+        int[] results = s.twoSum(nums, target);
+        for (int i = 0; i < results.length; i++) {
+            System.out.println(results[i]);
+        }
+    }
+
+    // 初步的暴力解决
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] results = { -1, -1 };
+//        for (int i = 0; i < nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                if (nums[i] + nums[j] == target) {
+//                    results[0] = i;
+//                    results[1] = j;
+//                    return results;
+//                } else {
+//                    continue;
+//                }
+//            }
+//        }
+//        return results;
+//    }
+
+    // 网上参考其他解法
+//    public int[] twoSum(int[] nums, int target) {
+//        int[] results = new int[2];
+//        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+//        for (int i = nums.length - 1; i >= 0; i--) {
+//            if (hm.containsKey(target - nums[i])) {
+//                results[0] = i;
+//                results[1] = hm.get(target - nums[i]);
+//                break;
+//            }
+//            hm.put(nums[i], i);
+//        }
+//        return results;
+//    }
+
+
+    // 网上参考其他解法改进
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(target - nums[i])) {
+                return new int[]{hm.get(target - nums[i]),i};
+            }
+            hm.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
