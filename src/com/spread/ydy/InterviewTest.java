17d330fd303d7b4371e8d3cf0cf7cac735bd43f2
devin.yin <devin.yin@SH02887NBW.spreadtrum.com>
2019-08-27 10:05:10 +0800

    Start think in java 4

---
+++ src/com/spread/ydy/InterviewTest.java
@@ -0,0 +1,13 @@
+package com.spread.ydy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InterviewTest {
+    public static void main(String[] args) {
+        Integer j = 5;
+        System.out.println(Integer.toBinaryString(j));
+        Integer i = -5;
+        System.out.println(Integer.toBinaryString(i));
+    }
+}
