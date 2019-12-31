ee8f24de10b616835edf0ec8c5b63b1756fab5a1
devin.yin <devin.yin@SH02887NBW.spreadtrum.com>
2019-12-31 10:20:29 +0800

    add chap15 generics

---
+++ src/com/spread/ydy/thkinjava/chap15Generic/Manipulator2.java
@@ -0,0 +1,15 @@
+package com.spread.ydy.thkinjava.chap15Generic;
+
+//: generics/Manipulator2.java
+
+class Manipulator2<T extends HasF> {
+    private T obj;
+
+    public Manipulator2(T x) {
+        obj = x;
+    }
+
+    public void manipulate() {
+        obj.f();
+    }
+} ///:~
