e4628ab395b494a1038f76aeee2080055b2e20aa
devin.yin <devin.yin@SH02887NBW.spreadtrum.com>
2020-01-07 10:27:27 +0800

    add chap15

---
+++ src/com/spread/ydy/thkinjava/chap15Generic/ArrayOfGenericReference.java
@@ -0,0 +1,9 @@
+package com.spread.ydy.thkinjava.chap15Generic;
+
+//: generics/ArrayOfGenericReference.java
+
+class Generic<T> {}
+
+public class ArrayOfGenericReference {
+  static Generic<Integer>[] gia;
+} ///:~
