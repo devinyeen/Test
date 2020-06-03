be686e54dea2603d3c07e5275fcd6c737cad385f
devin.yeen@icloud.com <devin.yeen@icloud.com>
2020-06-03 09:44:30 +0800

    add chap19enum

---
+++ src/com/spread/ydy/thkinjava/utils/Course.java
@@ -0,0 +1,20 @@
+//: enumerated/menu/Course.java
+package com.spread.ydy.thkinjava.utils;
+import com.spread.ydy.thkinjava.chap19enum.Food;
+
+public enum Course {
+    APPETIZER(Food.Appetizer.class),
+    MAINCOURSE(Food.MainCourse.class),
+    DESSERT(Food.Dessert.class),
+    COFFEE(Food.Coffee.class);
+
+    private Food[] values;
+
+    private Course(Class<? extends Food> kind) {
+        values = kind.getEnumConstants();
+    }
+
+    public Food randomSelection() {
+        return Enums.random(values);
+    }
+} ///:~
