e48ce4a1e47745956030c517e82de65bfbc90b4d
devin.yeen@icloud.com <devin.yeen@icloud.com>
2020-07-29 10:08:50 +0800

    add chap21thread

---
+++ src/com/spread/ydy/thkinjava/chap21thread/BasicThreads.java
@@ -0,0 +1,37 @@
+package com.spread.ydy.thkinjava.chap21thread;
+
+//: concurrency/BasicThreads.java
+// The most basic use of the Thread class.
+class LiftOff implements Runnable {
+    protected int countDown = 10; // Default
+    private static int taskCount = 0;
+    private final int id = taskCount++;
+
+    public LiftOff() {}
+
+    public LiftOff(int countDown) {
+        this.countDown = countDown;
+    }
+
+    public String status() {
+        return "#" + id + "(" + (countDown > 0 ? countDown : "Liftoff!") + "), ";
+    }
+
+    public void run() {
+        while (countDown-- > 0) {
+            System.out.print(status());
+            Thread.yield();
+        }
+    }
+}
+
+public class BasicThreads {
+    public static void main(String[] args) {
+        Thread t = new Thread(new LiftOff());
+        t.start();
+        System.out.println("Waiting for LiftOff");
+    }
+} /* Output: (90% match)
+Waiting for LiftOff
+#0(9), #0(8), #0(7), #0(6), #0(5), #0(4), #0(3), #0(2), #0(1), #0(Liftoff!),
+*///:~
