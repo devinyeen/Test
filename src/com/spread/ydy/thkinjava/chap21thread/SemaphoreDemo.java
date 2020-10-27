75995f15931610b0409e5e51fc6a1914c260edbf
devin.yeen@icloud.com <devin.yeen@icloud.com>
2020-10-27 09:55:30 +0800

    add chap21thread

---
+++ src/com/spread/ydy/thkinjava/chap21thread/SemaphoreDemo.java
@@ -0,0 +1,151 @@
+package com.spread.ydy.thkinjava.chap21thread;
+
+import static com.spread.ydy.thkinjava.utils.Print.print;
+import static com.spread.ydy.thkinjava.utils.Print.printnb;
+
+import java.util.ArrayList;
+import java.util.List;
+//: concurrency/SemaphoreDemo.java
+// Testing the Pool class
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+// A task to check a resource out of a pool:
+class CheckoutTask<T> implements Runnable {
+    private static int counter = 0;
+    private final int id = counter++;
+    private Pool<T> pool;
+
+    public CheckoutTask(Pool<T> pool) {
+        this.pool = pool;
+    }
+
+    public void run() {
+        try {
+            T item = pool.checkOut();
+            print(this + "checked out " + item);
+            TimeUnit.SECONDS.sleep(1);
+            print(this + "checking in " + item);
+            pool.checkIn(item);
+        } catch (InterruptedException e) {
+            // Acceptable way to terminate
+        }
+    }
+
+    public String toString() {
+        return "CheckoutTask " + id + " ";
+    }
+}
+
+public class SemaphoreDemo {
+    final static int SIZE = 25;
+
+    public static void main(String[] args) throws Exception {
+        final Pool<Fat> pool = new Pool<Fat>(Fat.class, SIZE);
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < SIZE; i++)
+            exec.execute(new CheckoutTask<Fat>(pool));
+        print("All CheckoutTasks created");
+        List<Fat> list = new ArrayList<Fat>();
+        for (int i = 0; i < SIZE; i++) {
+            Fat f = pool.checkOut();
+            printnb(i + ": main() thread checked out ");
+            f.operation();
+            list.add(f);
+        }
+        Future<?> blocked = exec.submit(new Runnable() {
+            public void run() {
+                try {
+                    // Semaphore prevents additional checkout,
+                    // so call is blocked:
+                    pool.checkOut();
+                } catch (InterruptedException e) {
+                    print("checkOut() Interrupted");
+                }
+            }
+        });
+        TimeUnit.SECONDS.sleep(2);
+        blocked.cancel(true); // Break out of blocked call
+        print("Checking in objects in " + list);
+        for (Fat f : list)
+            pool.checkIn(f);
+        for (Fat f : list)
+            pool.checkIn(f); // Second checkIn ignored
+        exec.shutdown();
+    }
+} /* (Execute to see output) *///:~
+
+class Pool<T> {
+    private int size;
+    private List<T> items = new ArrayList<T>();
+    private volatile boolean[] checkedOut;
+    private Semaphore available;
+
+    public Pool(Class<T> classObject, int size) {
+        this.size = size;
+        checkedOut = new boolean[size];
+        available = new Semaphore(size, true);
+        // Load pool with objects that can be checked out:
+        for (int i = 0; i < size; ++i)
+            try {
+                // Assumes a default constructor:
+                items.add(classObject.newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+    }
+
+    public T checkOut() throws InterruptedException {
+        available.acquire();
+        return getItem();
+    }
+
+    public void checkIn(T x) {
+        if (releaseItem(x))
+            available.release();
+    }
+
+    private synchronized T getItem() {
+        for (int i = 0; i < size; ++i)
+            if (!checkedOut[i]) {
+                checkedOut[i] = true;
+                return items.get(i);
+            }
+        return null; // Semaphore prevents reaching here
+    }
+
+    private synchronized boolean releaseItem(T item) {
+        int index = items.indexOf(item);
+        if (index == -1)
+            return false; // Not in the list
+        if (checkedOut[index]) {
+            checkedOut[index] = false;
+            return true;
+        }
+        return false; // Wasn't checked out
+    }
+} ///:~
+
+class Fat {
+    private volatile double d; // Prevent optimization
+    private static int counter = 0;
+    private final int id = counter++;
+
+    public Fat() {
+        // Expensive, interruptible operation:
+        for (int i = 1; i < 10000; i++) {
+            d += (Math.PI + Math.E) / (double) i;
+        }
+    }
+
+    public void operation() {
+        System.out.println(this);
+    }
+
+    public String toString() {
+        return "Fat id: " + id;
+    }
+} 
